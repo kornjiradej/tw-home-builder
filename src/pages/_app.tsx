f51d9dcc217b91f6c22b0bc1483b233eaacb97c5
(Korn)Jiradej Blockfint <jiradej@blockfint.com>
2023-04-02 22:24:57 +0700

    Updated  UI template

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,51 @@
-import '@/styles/globals.css'
+import { ReactElement, ReactNode, useState } from 'react'
+import { AppProps } from 'next/app'
+import { NextPage } from 'next'
+import {
+	MantineProvider,
+	ColorSchemeProvider,
+	ColorScheme,
+} from '@mantine/core'
+import Head from 'next/head'
 
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import { MantineProvider } from '@mantine/core';
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+	getLayout?: (page: ReactElement) => ReactNode
+}
 
-export default function App(props: AppProps) {
-  const { Component, pageProps } = props;
+type AppPropsWithLayout = AppProps & {
+	Component: NextPageWithLayout
+}
 
-  return (
-    <>
-      <Head>
-        <title>Page title</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+	const getLayout = Component.getLayout ?? ((page) => page)
 
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: 'dark',
-        }}
-      >
-        <Component {...pageProps} />
-      </MantineProvider>
-    </>
-  );
+	const [colorScheme, setColorScheme] = useState<ColorScheme>('light')
+	const toggleColorScheme = (value?: ColorScheme) =>
+		setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
+
+	return (
+		<>
+			<Head>
+				<title>Page title</title>
+				<meta
+					name='viewport'
+					content='minimum-scale=1, initial-scale=1, width=device-width'
+				/>
+			</Head>
+			<ColorSchemeProvider
+				colorScheme={colorScheme}
+				toggleColorScheme={toggleColorScheme}
+			>
+				<MantineProvider
+					withGlobalStyles
+					withNormalizeCSS
+					theme={{
+						colorScheme,
+					}}
+				>
+					{getLayout(<Component {...pageProps} />)}
+				</MantineProvider>
+			</ColorSchemeProvider>
+		</>
+	)
 }
