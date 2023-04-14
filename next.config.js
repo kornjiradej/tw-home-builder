7ecdf6664c929f248276ea0718af493bd39e1d56
(Korn)Jiradej Blockfint <kornjiradej@gmail.com>
2023-04-14 15:54:57 +0700

    Updated next config

--- next.config.js
+++ next.config.js
@@ -1,25 +1,15 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
-module.exports = (phase) => {
-	/**
-	 * @type {import('next').NextConfig}
-	 */
-	let nextConfig = {
-		/* config options here */
-		reactStrictMode: true,
-		env: {
-			basePath: 'tw-home-builder',
-		},
-	}
-	if (phase === PHASE_DEVELOPMENT_SERVER) {
-		nextConfig = {
-			/* development only config options here */
-			...nextConfig,
-			env: {
-				basePath: '',
-			},
-		}
-	}
-
-	return nextConfig
+let nextConfig = {
+	/* config options here */
+	reactStrictMode: true,
+	env: {
+		basePath: PHASE_DEVELOPMENT_SERVER ? '' : 'tw-home-builder',
+	},
 }
+
+module.exports = nextConfig
