80f33d0f9a698772cb7e00976af1d46841cecdf8
(Korn)Jiradej Blockfint <kornjiradej@gmail.com>
2023-04-15 09:16:58 +0700

    Updated Next Config

--- next.config.js
+++ next.config.js
@@ -1,33 +1,12 @@
-const {
-	PHASE_DEVELOPMENT_SERVER,
-	PHASE_PRODUCTION_BUILD,
-} = require('next/constants')
+/**
+ * @type {import('next').NextConfig}
+ */
 
-// This uses phases as outlined here: https://nextjs.org/docs/#custom-configuration
-module.exports = (phase) => {
-	// when started in development mode `next dev` or `npm run dev` regardless of the value of STAGING environment variable
-	const isDev = phase === PHASE_DEVELOPMENT_SERVER
-	// when `next build` or `npm run build` is used
-	const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1'
-	// when `next build` or `npm run build` is used
-	const isStaging =
-		phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === '1'
-
-	console.log(`isDev:${isDev}  isProd:${isProd}   isStaging:${isStaging}`)
-
-	const env = {
-		BASE_PATH: (() => {
-			if (isDev) return ''
-			if (isStaging) return 'tw-home-builder'
-			if (isProd) {
-				return 'tw-home-builder'
-			}
-			return 'tw-home-builder'
-		})(),
-	}
-
-	// next.config.js object
-	return {
-		env,
-	}
+const nextConfig = {
+	/* config options here */
+	env: {
+		BASE_PATH: process.env.BASE_PATH,
+	},
 }
+
+module.exports = nextConfig
